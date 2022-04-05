8754b9ab9f8c668e04bb89a546d4489469928af2
Emirhan <eKahya22@gmail.com>
2022-04-05 10:00:56 +0300

    Last Update 05/04/2022 - 10:00 AM

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -33,141 +33,182 @@ class _HomeScreenState extends State<HomeScreen> {
         width: double.maxFinite,
         height: double.maxFinite,
         margin: const EdgeInsets.only(top: 65, left: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                mLargeText(
-                  text: "Shoes Collections",
-                  color: AppColor.theme1,
-                  textSize: 22,
-                ),
-                SizedBox(height: 3),
-                mText(
-                  text: "The best of shoes in one place",
-                  color: AppColor.theme1.withOpacity(0.7),
-                  textSize: 16,
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.only(right: 30),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Container(
-                    width: size.width / 1.6,
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: AppColor.theme1.withOpacity(0.1),
-                          blurRadius: 20,
-                          offset: const Offset(1, 1),
-                        ),
-                      ],
-                    ),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.search,
-                            color: AppColor.theme1.withOpacity(0.6), size: 28),
-                        hintText: "Search",
-                        hintMaxLines: 1,
-                        hintStyle: TextStyle(
-                            fontSize: 20,
-                            color: AppColor.theme1.withOpacity(0.6)),
-                        fillColor: AppColor.theme4,
-                        filled: true,
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(50),
-                            borderSide: BorderSide(
-                              color: AppColor.theme4,
-                            )),
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(50),
-                            borderSide: BorderSide(
-                              color: AppColor.theme4,
-                            )),
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(50),
-                            borderSide: BorderSide(
-                              color: AppColor.theme4,
-                            )),
-                      ),
-                    ),
+                  mLargeText(
+                    text: "Shoes Collections",
+                    color: AppColor.theme1,
+                    textSize: 22,
                   ),
-                  Container(
-                    width: 60,
-                    height: 60,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50),
-                      color: AppColor.theme1,
-                      boxShadow: [
-                        BoxShadow(
-                          color: AppColor.theme1.withOpacity(0.8),
-                          blurRadius: 20,
-                          offset: const Offset(1, 1),
-                        ),
-                      ],
-                    ),
-                    child: Center(
-                      child: Icon(Icons.sort_rounded,
-                          color: AppColor.theme4, size: 28),
-                    ),
+                  SizedBox(height: 3),
+                  mText(
+                    text: "The best of shoes in one place",
+                    color: AppColor.theme1.withOpacity(0.7),
+                    textSize: 16,
                   ),
                 ],
               ),
-            ),
-            SizedBox(height: 20),
-            Container(
-              height: 70,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: _data.length,
-                itemBuilder: (context, index) {
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.only(right: 30),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: size.width / 1.6,
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: AppColor.theme1.withOpacity(0.1),
+                            blurRadius: 20,
+                            offset: const Offset(1, 1),
+                          ),
+                        ],
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.search,
+                              color: AppColor.theme1.withOpacity(0.6), size: 28),
+                          hintText: "Search",
+                          hintMaxLines: 1,
+                          hintStyle: TextStyle(
+                              fontSize: 20,
+                              color: AppColor.theme1.withOpacity(0.6)),
+                          fillColor: AppColor.theme4,
+                          filled: true,
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(50),
+                              borderSide: BorderSide(
+                                color: AppColor.theme4,
+                              )),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(50),
+                              borderSide: BorderSide(
+                                color: AppColor.theme4,
+                              )),
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(50),
+                              borderSide: BorderSide(
+                                color: AppColor.theme4,
+                              )),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 60,
+                      height: 60,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: AppColor.theme1,
+                        boxShadow: [
+                          BoxShadow(
+                            color: AppColor.theme1.withOpacity(0.8),
+                            blurRadius: 20,
+                            offset: const Offset(1, 1),
+                          ),
+                        ],
+                      ),
+                      child: Center(
+                        child: Icon(Icons.sort_rounded,
+                            color: AppColor.theme4, size: 28),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                height: 70,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: _data.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: const EdgeInsets.only(right: 15),
+                      child: FilledButton(
+                        press: () {
+                          setState(() {
+                            _data[index].isFilled = true;
+                          });
+                        },
+                        text: _data[index].name,
+                        images: _data[index].logo,
+                        isFilled: _data[index].isFilled,
+                      ),
+                    );
+                  },
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                width: size.width,
+                height: 400,
+                color: AppColor.theme4,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 5,
+                  itemBuilder: (context, int){
                   return Container(
-                    margin: const EdgeInsets.only(right: 15),
-                    child: FilledButton(
-                      press: () {
-                        setState(() {
-                          _data[index].isFilled = true;
-                        });
-                      },
-                      text: _data[index].name,
-                      images: _data[index].logo,
-                      isFilled: _data[index].isFilled,
+                    margin: const EdgeInsets.only(right: 20),
+                    child: ShoesCard(
+                      name: "Nike Air Max",
+                      image: "assets/images/shoes1.png",
+                      price: "250",
+                      colorCount: "5",
+                      type: 'Men',
                     ),
                   );
-                },
+                },),
               ),
-            ),
-            SizedBox(height: 20),
-            Container(
-              width: size.width,
-              height: 400,
-              color: AppColor.theme4,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: 5,
-                itemBuilder: (context, int){
-                return Container(
-                  margin: const EdgeInsets.only(right: 20),
-                  child: ShoesCard(
-                    name: "Nike Air Max",
-                    image: "assets/images/shoes1.png",
-                    price: "250",
-                    colorCount: "5",
-                    type: 'Men',
-                  ),
-                );
-              },),
-            ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Container(
+        height: 70,
+        width: double.maxFinite,
+        decoration: BoxDecoration(
+          color: AppColor.theme4,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(10),
+            topRight: Radius.circular(10),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            buildContainerBottomNav(Icons.home),
+            buildContainerBottomNav(Icons.textsms_outlined),
+            buildContainerBottomNav(Icons.shopping_cart, isSelected: true),
+            buildContainerBottomNav(Icons.notifications_none),
+            buildContainerBottomNav(Icons.person),
           ],
         ),
       ),
     );
+  }
+
+  Container buildContainerBottomNav(IconData icon,{isSelected = false}) {
+    return Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+              color: isSelected ? AppColor.theme1.withOpacity(0.8) : AppColor.theme4,
+              shape: BoxShape.circle,
+              boxShadow: isSelected ? [
+                BoxShadow(
+                  color: AppColor.theme1.withOpacity(0.1), blurRadius: 10, spreadRadius: 1
+                )
+              ] : [],
+            ),
+            child: Icon(icon, color: isSelected ? AppColor.theme4 : AppColor.theme1,),
+          );
   }
 }
